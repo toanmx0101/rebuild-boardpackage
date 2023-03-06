4954761200f03f39c1e7e37f6ff263ad96f428b3
Toan Mai <wth8647@gmail.com>
2023-03-06 12:53:19 +0700

    feat: 🎸 add images

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,5 @@
 class Building < ApplicationRecord
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@
 Rails.application.routes.draw do
   resources :buildings
   devise_for :users
-  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'tasks#index'
+  root 'buildings#index'
 end
